f43c0399cff5afc581f1d14d6274b291e6c1f1b0
Maria Del Castillo <maria.f.delcastillo@gmail.com>
2017-07-15 20:51:34 -0700

    followed instructions, nothing works

---
+++ src/app/api-keys.ts
@@ -0,0 +1,1 @@
+export const marsRoverKey = '{3GDYeXCFjODCmKHxb2R9pfdHB3tqlGdHaiB2mIzv}'
---
+++ src/app/rover-form/rover-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { MarsRoverAPIPhotos } from './../mars-rover-api-photos.service';
+
+@Component({
+  selector: 'app-rover-form',
+  templateUrl: './rover-form.component.html',
+  styleUrls: ['./rover-form.component.css'],
+  providers: [MarsRoverAPIPhotos]
+})
+export class RoverFormComponent implements OnInit {
+  photos: any[] = null;
+  noPhotos: boolean = false;
+
+  constructor(
+    private router: Router,
+    private marsAPI: MarsRoverAPIPhotos
+  ) {}
+
+  getRoverImages(date: string, camera: string): void {
+    this.noPhotos = false;
+    this.marsAPI.getByDateAndCamera(date, camera).subscribe(data => {
+      if (data.json().photos.length) {
+        this.photos = data.json();
+      }
+      else this.noPhotos = true;
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
