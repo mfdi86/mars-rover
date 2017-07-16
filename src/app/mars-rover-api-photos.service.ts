a7d47172620bb2229ea1ff7f1ee634338345ba3f
Maria Del Castillo <maria.f.delcastillo@gmail.com>
2017-07-15 21:04:45 -0700

    fix rover form

--- src/app/mars-rover-api-photos.service.ts
+++ src/app/mars-rover-api-photos.service.ts
@@ -4,7 +4,7 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class MarsRoverApiPhotosService {
+export class MarsRoverAPIPhotos {
 
   constructor(private http: Http) { }
 
